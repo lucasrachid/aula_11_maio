25a645fe02b5d2cd54ce05175a8e30b58bbe7f29
Lucas Rachid Martins <lucasrachid@hotmail.com>
2023-06-01 22:19:08 -0300

    adicionando supabase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,33 @@
 import 'package:aula_onze_maio/app.dart';
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+// import 'package:mysql1/mysql1.dart';
 
-void main() {
+
+Future<void> main() async {
+  await Supabase.initialize(
+    url: 'https://bolegsosusjeptueocek.supabase.co',
+    // url: 'https://localhost:3000',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJvbGVnc29zdXNqZXB0dWVvY2VrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODU2NjYwNDQsImV4cCI6MjAwMTI0MjA0NH0.9i_ocwi4N3Tfe6D8UB4Vm8IS3g93lemsY3JN56PRDJs',
+  );
+
+  final response = await Supabase.instance.client.from('users').select('*');
+
+  if (response.error == null) {
+    final List<dynamic>? data = response.data;
+    if (data != null) {
+      for (final row in data) {
+        print('ID: ${row['id']}, Nome: ${row['nome']}, Idade: ${row['idade']}');
+      }
+    }
+  }
+
   runApp(const App());
 }
+
+
+
+
 
   // Vantagens Banco Fake
   // Trabalho em equipe
--- lib/rota.dart
+++ lib/rota.dart
@@ -1,4 +1,4 @@
-class Rota{
+class Rota {
   static const String contatoLista = '/';
   static const String contatoForm = 'contato_form';
   static const String contatoDetalhe = 'contato_detalhe';
